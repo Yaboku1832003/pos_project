82a6dd26a31e9d9e2c547e78cd1a4eb13ff3dfdd
Yaboku1832003 <kozwewaiyan8@gmail.com>
2025-08-10 02:23:50 +0630

    Update admin.php

--- routes/admin.php
+++ routes/admin.php
@@ -1,10 +1,10 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\AdminController;
+use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\ProductController;
 use App\Http\Controllers\Admin\ProfileController;
-use App\Http\Controllers\Admin\CategoryController;
+use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'admin', 'middleware' => 'adminMiddleware'], function () {
     Route::get('dashboard', [AdminController::class, 'dashboard'])->name('admin#dashboard');
@@ -12,7 +12,9 @@ Route::group(['prefix' => 'admin', 'middleware' => 'adminMiddleware'], function 
     Route::group(['prefix' => 'category'], function () {
         Route::get('list', [CategoryController::class, 'list'])->name('category#list');
         Route::post('create', [CategoryController::class, 'create'])->name('category#create');
+
         Route::get('delete/{id}', [CategoryController::class, 'delete'])->name('category#delete');
+
         Route::get('edit/{id}', [CategoryController::class, 'edit'])->name('category#edit');
         Route::post('update/{id}', [CategoryController::class, 'update'])->name('category#update');
     });
@@ -20,16 +22,47 @@ Route::group(['prefix' => 'admin', 'middleware' => 'adminMiddleware'], function 
     Route::group(['prefix' => 'product'], function () {
         Route::get('createPage', [ProductController::class, 'categoryList'])->name('product#createPage');
         Route::post('create', [ProductController::class, 'create'])->name('product#create');
+
         Route::get('delete/{id}', [ProductController::class, 'delete'])->name('product#delete');
+
         Route::get('edit/{id}', [ProductController::class, 'edit'])->name('product#edit');
         Route::post('update', [ProductCOntroller::class, 'update'])->name('product#update');
+
         Route::get('list/{action?}', [ProductController::class, 'list'])->name('product#list');
         Route::get('lowAmt', [ProductController::class, 'lowAmount'])->name('product#lowAmt');
 
     });
 
-    Route::group(['prefix' => 'profile'], function() {
-        Route::get('change/password',[ProfileController::class,'changePasswordPage'])->name('profile#changePassword');
-        Route::post('change/password',[ProfileController::class,'changePassword'])->name('profile#changePassword');
+    Route::group(['prefix' => 'profile'], function () {
+        Route::get('change/password', [ProfileController::class, 'changePasswordPage'])->name('profile#changePassword');
+        Route::post('change/password', [ProfileController::class, 'changePassword'])->name('profile#changePassword');
+
+        Route::get('edit', [ProfileController::class, 'editProfile'])->name('profile#edit');
+        Route::post('update', [ProfileController::class, 'updateProfile'])->name('profile#update');
+    });
+
+    Route::middleware('superAdminMiddleware')->group(function () {
+        //one
+        Route::prefix('payment')->group(function () {
+            Route::get('createPage', [AdminController::class, 'createPaymentMethodPage'])->name('payment#paymentMethod');
+            Route::post('create/method', [AdminController::class, 'createMethod'])->name('payment#storeMethod');
+
+            Route::get('delete/{id}',[AdminController::class,'deleteMethod'])->name('payment#delete');
+
+            Route::get('edit/{id}',[AdminController::class,'editMethod'])->name('payment#edit');
+            Route::post('update/{id}',[AdminController::class,'updateMethod'])->name('payment#update');
+
+        });
+
+        //two
+        //one and two are the same with different writing style
+
+        Route::group(['prefix' => 'account'], function () {
+            Route::get('create/newAdmin', [AdminController::class, 'createAdminPage'])->name('account#newAdmin');
+            Route::get('admin/list', [AdminController::class, 'adminList'])->name('account#adminList');
+            Route::get('user/list', [AdminController::class, 'userList'])->name('account#userList');
+
+        });
+
     });
 });
